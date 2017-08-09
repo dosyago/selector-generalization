e8d4b322f4e43f5ccc6123cb7be9d47ed378b1fe
Cris <cris@dosaygocorporation>
2017-08-09 21:34:23 +0800

    Added visual key to display algorithm results

--- test.js
+++ test.js
@@ -1,11 +1,16 @@
 "use strict";
 {
   const sg = require('./lib.js');
+  const navigable = new Set([
+    'A',
+    'BUTTON'
+  ]);
   const test = {
     run
   };
 
   module.exports = test;
+
   Object.assign( self, { test } );
 
   // one possible test
@@ -20,20 +25,41 @@
   function run() {
     console.log("Running tests...");
     const set = new Set();
+    let found = [];
     document.addEventListener('click', e => {
-      if ( e.target.tagName == 'A' ) {
+      if ( navigable.has( e.target.tagName ) ) {
         e.preventDefault();
       }
       if( e.target.id == 'generalize' ) {
         const sel = sg.generalize( [...set] );
-        const found = Array.from( document.querySelectorAll(sel) );
+        found.forEach( el => {
+          el.style.filter = "none";
+          el.style.background = "none";
+        });
+        found = Array.from( document.querySelectorAll(sel) );
         const result = validate( set, found );
         console.log(" Test result?", result );
         console.log( sel, set, found );
+        Array.from( set ).forEach( el => {
+          el.style.outline = "3px dashed lime";
+        });
+        found.forEach( el => {
+          el.style.filter = "sepia(1)";
+          el.style.background = "lime";
+        });
       } else if ( e.target.id == 'clear' ) {
+        Array.from( set ).forEach( el => {
+          el.style.outline = "none";
+        });
+        found.forEach( el => {
+          el.style.filter = "none";
+          el.style.background = "none";
+        });
         set.clear();
       } else {
         set.add( e.target );
+        console.log(e.target.dataset.outline);
+        e.target.style.outline = "2px solid lime";
       }
     });
   }
