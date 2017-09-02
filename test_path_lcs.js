1a2794c5fd5ae95bd373b4ec4ebf5123338b332e
Cris <cris@dosaygocorporation>
2017-09-02 17:25:26 +0800

    First attempt at canonical path from selector

---
+++ test_path_lcs.js
@@ -0,0 +1,11 @@
+"use strict";
+{
+  const p = require('./path_lcs.js');
+
+  run();
+
+  function run() {
+    const sel = 'tag#id.class1.class2:any(an1, an2):nth-of-type(geo)';
+    console.log( p.path_from_sel(sel) );
+  }
+}
